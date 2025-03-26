8c91cd751e29978afb573b477c18f5fca3750652
mehran <taghavimehran99@gmail.com>
2025-03-26 11:01:57 +0330

    fix styels in mobile hero section in project

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,9 @@
 "use strict";
 let c = 10;
+let m = 77;
+const sum = c + m;
+console.log(sum);
+function fun(pp) {
+    return 10 + 'pp';
+}
+console.log(fun(10));
--- sandbox.ts
+++ sandbox.ts
@@ -1,1 +1,11 @@
 let c=10;
+let m=77;
+const sum=c+m;
+console.log(sum);
+
+
+function fun(pp) {
+    return 10+'pp';
+    
+}
+console.log(fun(10));
